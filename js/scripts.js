0ab08d3d4e3746cbac080714e93c002f6d7467b0
Amy Hwang and Jared Alford <jared@>
2016-10-19 09:57:09 -0700

    add jquery to change the alert effects to output text in user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,30 @@
-/*var add = function(number1, number2) {
+var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+
 
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = subtract(number1, number2);
-alert(result);
 
 var mutiplication = function(number1, number2) {
   return number1 * number2
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = mutiplication(number1, number2);
-alert(result);*/
+
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
